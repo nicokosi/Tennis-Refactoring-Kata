2c212a5b0c72f20a205f4a9dd63a8457b1688ee8
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2023-02-12 19:50:24 +0100

    Java: use switch statement

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -35,18 +35,10 @@ public class TennisGame1 implements TennisGame {
                     tempScore = player2Score;
                 }
                 switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
+                    case 0 -> score += "Love";
+                    case 1 -> score += "Fifteen";
+                    case 2 -> score += "Thirty";
+                    case 3 -> score += "Forty";
                 }
             }
         }
